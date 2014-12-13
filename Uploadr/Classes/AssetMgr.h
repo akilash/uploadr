c2d6d26c86fa9fdca24f9aee33cf22ab3701009d
Kervins Valcourt <kervins.valcourt@gmail.com>
2014-12-12 20:29:05 -0500

    adding the asset manager file

---
+++ Uploadr/Classes/AssetMgr.h
@@ -0,0 +1,13 @@
+//
+//  AssetMgr.h
+//  
+//
+//  Created by Kervins Valcourt on 12/12/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AssetMgr : NSObject
+
+@end
---
+++ Uploadr/Classes/AssetMgr.m
@@ -0,0 +1,13 @@
+//
+//  AssetMgr.m
+//  
+//
+//  Created by Kervins Valcourt on 12/12/14.
+//
+//
+
+#import "AssetMgr.h"
+
+@implementation AssetMgr
+
+@end
