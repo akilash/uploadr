2effba92cb15aed5b1549fc6b72e2866fe0bb49a
kervs <kervins.valcourt@gmail.com>
2015-01-16 01:37:52 -0500

    sorting assets array

--- Uploadr/ViewController.m
+++ Uploadr/ViewController.m
@@ -7,21 +7,90 @@
 //
 
 #import "ViewController.h"
+#import <Foundation/Foundation.h>
+#import <Photos/Photos.h>
 
 @interface ViewController ()
+
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSMutableArray * assets = [[NSMutableArray alloc]init];
+    PHFetchResult * moments = [PHAssetCollection fetchMomentsWithOptions:nil];
+    
+    NSUInteger momentsCount = moments.count;
+    
+    for (PHAssetCollection * moment in moments) {
+        
+        PHFetchResult * assetsFetchResults = [PHAsset fetchAssetsInAssetCollection:moment options:nil];
+        
+        __block NSUInteger pendingEnumerations = assetsFetchResults.count;// * momentsCount;
+
+        for (PHAsset * asset in assetsFetchResults) {
+            
+            [[PHImageManager defaultManager] requestImageDataForAsset:asset options:nil resultHandler:^(NSData *imageData, NSString *dataUTI, UIImageOrientation orientation, NSDictionary *info) {
+                
+                [assets addObject:imageData];
+                
+                NSUInteger imageSize = imageData.length;
+                
+                NSLog(@"%lu",(unsigned long)imageSize);
+                
+                pendingEnumerations--;
+                
+                if (pendingEnumerations <= 0) {
+                    
+                    NSArray * sortedAssets =  [self quickSort:assets];
+                    
+                    NSLog(@"sortedAssets: %@",sortedAssets);
+                }
+                
+            }];
+            
+        }
+        
+        
+        
+    }
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSArray *)quickSort:(NSMutableArray *)unsortedDataArray
+{
+    
+    NSMutableArray *lessArray = [NSMutableArray new] ;
+    NSMutableArray *greaterArray =[NSMutableArray new] ;
+    if ([unsortedDataArray count] <1)
+    {
+        return nil;
+    }
+    int randomPivotPoint = arc4random() % [unsortedDataArray count];
+    NSData *pivotValue = [unsortedDataArray objectAtIndex:randomPivotPoint];
+    [unsortedDataArray removeObjectAtIndex:randomPivotPoint];
+    for (NSData *imageData in unsortedDataArray)
+    {
+       
+        if (imageData.length < pivotValue.length)
+        {
+            [lessArray addObject:imageData];
+        }
+        else
+        {
+            [greaterArray addObject:imageData];
+        }
+    }
+    NSMutableArray *sortedArray = [NSMutableArray new];
+    [sortedArray addObjectsFromArray:[self quickSort:lessArray]];
+    [sortedArray addObject:pivotValue];
+    [sortedArray addObjectsFromArray:[self quickSort:greaterArray]];
+    return sortedArray;
 }
 
 @end
